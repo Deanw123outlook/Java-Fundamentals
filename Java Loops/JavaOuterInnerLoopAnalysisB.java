ba6d74133e65fa735308b45d7d5f7c56c5573645
Dean Watters <79112001+Deanw123outlook@users.noreply.github.com>
2022-09-23 19:49:38 +0100

    Add files via upload

--- Java Loops/JavaOuterInnerLoopAnalysisB.java
+++ Java Loops/JavaOuterInnerLoopAnalysisB.java
@@ -1,18 +1,35 @@
 public class JavaOuterInnerLoopAnalysisB {
     public static void main(String[] args) {
         // initiate array of elements
-        int[] items = {10,20,30,40};
-        // initiate empty array
-        // Loop 1
-        for (int i = 0; i < (items.length); i++){
+        int[] items = {10, 20, 30, 40};
+        // Declare variable that can be accessed inside loop & incremented
+        int iteration = 0;
+        // Outer Loop
+        for (int i = 0; i < (items.length); i++) {
+            //
+            iteration++; //increment variable
+            System.out.println("Program-Iteration-Analysis : " + iteration);
+            // Outer Loop
             System.out.println("Outer-Loop");
             System.out.println("Index : " + i);
             System.out.println(items[i]);
-            for (int x = 1; x < items.length; x++){
+            // Inner Loop
+            for (int x = 1; x < 2; x++) {
                 System.out.println("Inner-Loop");
-                System.out.println("Index : " + x);
-                System.out.println(items[x]);
+                System.out.println("Index : " + iteration);
+                // if-else statement
+                if (iteration < items.length) {
+                    System.out.println(items[iteration]);
+                }
+                else
+                {
+                    System.out.println("Index-out-of-range");
+                    System.out.println("Break-Loop");
+                    // break statement will exit loop at the current iteration
+                    break;
+                }
             }
         }
     }
 }
+
